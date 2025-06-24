e9ba374b1a4c983c29cfefc0aa779b01327d0961
technoplay.aiservers <technoplay.aiservers@gmail.com>
2025-06-24 23:19:26 +0000

    Update Navbar: Remove text next to logo and use logo.png from assets

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom";
 import { styles } from "../styles";
 import { navLinks } from "../constants";
 import { logo, menu, close } from "../assets";
-
+import logo from "../assets/logo.png"; // Corrected import path
 const Navbar = () => {
   const [active, setActive] = useState("");
   const [toggle, setToggle] = useState(false);
@@ -42,11 +42,7 @@ const Navbar = () => {
             window.scrollTo(0, 0);
           }}
         >
-          <img src={logo} alt='logo' className='w-9 h-9 object-contain' />
-          <p className='text-white text-[18px] font-bold cursor-pointer flex '>
-            Adrian &nbsp;
-            <span className='sm:block hidden'> | JavaScript Mastery</span>
-          </p>
+ <img src={logo} alt='logo' className='w-9 h-9 object-contain' />
         </Link>
 
         <ul className='list-none hidden sm:flex flex-row gap-10'>
