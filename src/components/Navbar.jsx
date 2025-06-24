24b3b037f7b00e2f17b9b3c6309df1c00a93e28a
technoplay.aiservers <technoplay.aiservers@gmail.com>
2025-06-24 22:29:49 +0000

    Fix: Display logo in Navbar by using raw GitHub URL

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -42,7 +42,7 @@ const Navbar = () => {
             window.scrollTo(0, 0);
           }}
         >
-          <img src="https://github.com/AIPlaza/Stock/blob/main/Isotipo_TechnoPlay.png" alt='logo' className='w-9 h-9 object-contain' />
+          <img src="https://raw.githubusercontent.com/AIPlaza/Stock/main/Isotipo_TechnoPlay.png" alt='logo' className='w-9 h-9 object-contain' />
         </Link>
 
         <ul className='list-none hidden sm:flex flex-row gap-10'>
