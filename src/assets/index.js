91c2f22154658964566b29941af1d107825e3f5f
technoplay.aiservers <technoplay.aiservers@gmail.com>
2025-06-25 01:42:10 +0000

    Add testimonial images to assets index

--- src/assets/index.js
+++ src/assets/index.js
@@ -30,6 +30,10 @@ import carrent from "./carrent.png";
 import jobit from "./jobit.png";
 import tripguide from "./tripguide.png";
 
+import harry from "./harry.png";
+import Frankie from "./Frankie.png";
+import Maria from "./Maria.png";
+
 export {
   logo,
   backend,
@@ -59,4 +63,7 @@ export {
   carrent,
   jobit,
   tripguide,
+  harry,
+  Frankie,
+  Maria,
 };
